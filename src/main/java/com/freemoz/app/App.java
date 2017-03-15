cf2e370dba869fc0fb60c27b8725bf1958903371
Ben Boyter <ben@boyter.org>
2017-03-16 09:06:14 +1100

    Quick release

--- src/main/java/com/freemoz/app/App.java
+++ src/main/java/com/freemoz/app/App.java
@@ -36,7 +36,7 @@ public class App {
         get("/about/", (request, response) -> new ModelAndView(null, "about.ftl"), new FreeMarkerEngine());
         get("/become/", (request, response) -> new ModelAndView(null, "become.ftl"), new FreeMarkerEngine());
         get("/suggest/", (request, response) -> new ModelAndView(null, "suggest.ftl"), new FreeMarkerEngine());
-        post("/suggest/", (SuggestRoute::suggest), new FreeMarkerEngine());
+//        post("/suggest/", (SuggestRoute::suggest), new FreeMarkerEngine());
 
         // Special routes to preserve the root categories
         get("/Arts/*", (request, response) -> ContentRoute.getCategory(request, response, "Arts"), new FreeMarkerEngine());
@@ -68,11 +68,11 @@ public class App {
         //              API Routes Below
         ////////////////////////////////////////////////////
 
-        path("/api", () -> {
-            path("/v1", () -> {
-                get("/categories/", ContentRoute::getCategories, new JsonTransformer());
-            });
-        });
+//        path("/api", () -> {
+//            path("/v1", () -> {
+//                get("/categories/", ContentRoute::getCategories, new JsonTransformer());
+//            });
+//        });
 
 
     }
@@ -87,6 +87,6 @@ public class App {
         Singleton.getQueueDAO().createTableIfMissing();
 
         // Start jobs
-        //Singleton.getJobService().startJobs();
+        Singleton.getJobService().startJobs();
     }
 }
