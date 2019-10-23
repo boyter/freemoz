20c1d4c29886befcc77f81249634c9de0131bb83
Ben Boyter <ben@boyter.org>
2019-10-24 08:55:33 +1100

    Start to work with bindata

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/boyter/freemoz/handlers"
-	"github.com/boyter/pcs/service"
 	//"github.com/golangcollege/sessions"
 	"log"
 	"net/http"
 	"os"
 )
 
+//go:generate go-bindata ./assets/public/...
 func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+
+	data, err := Asset("./assets/public/js/script.js")
+	if err != nil {
+		// Asset was not found.
+	}
+
+	fmt.Println(string(data))
 
 	//db, err := data.ConnectDb()
 	//if err != nil {
@@ -37,13 +46,13 @@ func main() {
 	}
 
 	// Run background tasks
-	go service.RunBackground()
+	//go service.RunBackground()
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":8080")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
 	// we use the log.Fatal() function to log the error message and exit.
 	infoLog.Println("Starting server on :8080")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
